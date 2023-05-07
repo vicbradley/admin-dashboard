b8bc91c19370b73a35ed4d7638601321b1a885a1
vicbradley <vicbradley33@gmail.com>
2023-05-07 23:32:13 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// const tabs = document.querySelectorAll(".tab");
+// const icons = document.querySelectorAll("i");
+
+// // console.log(i)
+
+// // i.addEventListener('click',() => {
+// //     alert("helo")
+// // })
+
+// tabs.forEach(tab => {
+//     tab.addEventListener('mouseover',() => {
+//         icons.forEach(icon => {
+//             icon.classList.add("icon-hover")
+//         })
+//     })
+
+//     tab.addEventListener('mouseout',() => {
+//         icons.forEach(icon => {
+//             icon.classList.remove("icon-hover")
+//         })
+//     })
+// });
